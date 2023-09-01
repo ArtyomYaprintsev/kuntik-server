a5f8a856d832a901702b46a0c1db56591ae0aa1c
ArtyomYaprintsev <yapryntsev.a02@mail.ru>
2023-09-02 00:45:31 +0500

    Add store models to the django admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
 
-# Register your models here.
+from store import models
+
+
+@admin.register(models.Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ("name", "cost", )
+    list_display_links = ("name", )
+    sortable_by = ("name", "cost", )
+
+
+@admin.register(models.Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ("name", "cost", "destiny", )
+    list_display_links = ("name", )
+    sortable_by = ("name", "cost", )
+
+
+@admin.register(models.PreparedPurchase)
+class PreparedPurchaseAdmin(admin.ModelAdmin):
+    list_display = ("name", "color", "material", "price", )
+    sortable_by = ("color", "material", "price", )
+
+    @admin.display()
+    def name(self, obj):
+        return str(obj)
+
+
+class ConsultInline(admin.StackedInline):
+    model = models.Consult
+    extra = 0
+
+    def has_add_permission(self, *args, **kwargs) -> bool:
+        return False
+
+
+@admin.register(models.Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    inlines = [ConsultInline, ]
+
+    list_display = ("code", "state", "price", "color", "material", "date_created", )
+    sortable_by = ("state", "date_created", )
+
+    fieldsets = (
+        (None, {
+            "classes": ("wide", ),
+            "fields": (
+                "address", "state",
+            )
+        }),
+        ("Properties", {
+            "classes": ("wide", ),
+            "fields": (
+                "color", "material", "size",
+            )
+        }),
+    )
